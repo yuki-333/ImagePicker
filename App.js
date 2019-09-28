fcce85060941e7661c71246f55a228d8855e49f7
yuki nara <yukinara@NYnoMacBook-Air.local>
2019-09-28 19:25:21 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Container, View, Header, Left, Body, Right, Button, Title, Text, Thumbnail } from 'native-base';
+import ImagePicker from 'react-native-image-picker';
+
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       image: null,
+    }
+  }
+
+  showPicker = () => {
+    // import ImagePicker from 'react-native-image-picker';
+
+    let options = {
+      title: '画像を選択',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    }
+
+    ImagePicker.showImagePicker(options, (response)  => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else {
+        console.log(response.uri)
+        this.setState({image: response.uri})
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left />
+          <Body>
+            <Title>メイン</Title>
+          </Body>
+          <Right />
+        </Header>
+        <View>
+          <Button small iconRight  transparent primary onPress={this.showPicker}>
+            <Text>画像の選択</Text>
+          </Button>
+          {this.state.image && <Thumbnail square large source={{uri: this.state.image}} />}
+        </View>
+      </Container>
+    );
+  }
+}
